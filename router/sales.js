30b790fc177873ceaa889f3e00db278b6b342f60
rohits-web03 <rohit.dey302003@gmail.com>
2023-12-14 00:37:42 +0530

    Added the route for profit based on date and merchant_id

--- router/sales.js
+++ router/sales.js
@@ -38,6 +38,67 @@ router.get("/getmonthly", sales.getMonthlySales);
 router.get("/get/:userID/totalsaleamount", sales.getTotalSalesAmount);
 router.get("/get/:userID/totalprofit", sales.getTotalProfit);
 
+//Get Profit based on Day
+//Request Body:
+// {
+//   "merchant_id":"m50",
+//   "date":"11/8/2023"
+// }
+router.post('/profit_by_day', async (req, res) => {
+  try {
+    const { merchant_id, date } = req.body;
+
+    // Retrieve sales data from 'customer_sales'
+    const customerSales = await Sales.find({
+      merchant_id: merchant_id,
+      date_sold: date
+    });
+    console.log("Customer Sales:",customerSales)
+
+    // Collect product IDs and selling price per unit
+    const productSellPrice = {};
+    customerSales.forEach((sale) => {
+      productSellPrice[sale.product_id] = sale.selling_price_per_unit;
+    });
+
+    // Retrieve purchase data from 'merchant_purchases' based on product IDs
+    const products = Object.keys(productSellPrice);
+    const productCostPrice = {};
+
+    for (const product_id of products) {
+      const costPriceData = await Purchase.findOne({
+        merchant_id: merchant_id,
+        product_id: product_id
+      });
+
+      if (costPriceData) {
+        productCostPrice[product_id] = costPriceData.cost_price_per_unit;
+      } else {
+        console.log(`No cost price data found for product ID: ${product_id}`);
+      }
+    }
+
+    // Calculate profit for each product and accumulate total profit
+    const profits = [];
+    let totalProfit = 0;
+
+    for (const product_id in productSellPrice) {
+      if (productCostPrice[product_id] !== undefined) {
+        const unitsSold = customerSales.find(sale => sale.product_id === product_id).units_sold;
+        const profit = (productSellPrice[product_id] - productCostPrice[product_id]) * unitsSold;
+        totalProfit += profit;
+        profits.push({ product_id: product_id, profit: profit.toFixed(2) });
+      }
+    }
+
+    res.status(200).json({ profits: profits, total_profit: totalProfit.toFixed(2) });
+  } catch (error) {
+    console.error('Error while calculating profit:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 //Get monthly profit
 // The request looks like 
 // {
