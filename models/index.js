3dd4e67123b2ad6a91df843d93f0d6c08f8edbb0
rohits-web03 <rohit.dey302003@gmail.com>
2023-12-13 21:15:02 +0530

    Added environment variable for mongoDB connection url

--- models/index.js
+++ models/index.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
-//const uri = "mongodb+srv://sagnikpramanik95:LHEyHTCbO5oP5YSR@cluster0.lciunbh.mongodb.net/?retryWrites=true&w=majority";
-const uri = "mongodb+srv://bizminds:vVqmvCxpSI8fyerd@cluster0.mojhc2a.mongodb.net/db0?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URL;
 
 function main() {
     mongoose.connect(uri).then(() => {
-        console.log("Succesfull")
+        console.log("Connected to MongoDB Atlas Succesfully!!")
     
     }).catch((err) => {
-        console.log("Error: ", err)
+        console.log("Error: ", err);
     })
 }
 
