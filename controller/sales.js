dfab2ea8b41a920ec8e8d98694272f515ca078ec
rohits-web03 <rohit.dey302003@gmail.com>
2023-12-14 06:34:43 +0530

    Fixed the callbacks for all the routes

--- controller/sales.js
+++ controller/sales.js
@@ -2,6 +2,28 @@ const Sales = require("../models/sales");
 const soldStock = require("../controller/soldStock");
 const Purchase = require("../models/purchase");
 
+function getMonthNumber(monthName) {
+  const months = {
+      "January": 1,
+      "February": 2,
+      "March": 3,
+      "April": 4,
+      "May": 5,
+      "June": 6,
+      "July": 7,
+      "August": 8,
+      "September": 9,
+      "October": 10,
+      "November": 11,
+      "December": 12
+  };
+
+  // Convert the month name to title case (e.g., "january" to "January")
+  const formattedMonth = monthName.charAt(0).toUpperCase() + monthName.slice(1).toLowerCase();
+
+  return months[formattedMonth] || null;
+}
+
 // Add Sales data
 const addSales=async (req, res) => {
   try {
@@ -32,59 +54,226 @@ const addSales=async (req, res) => {
 
 // Get All Sales Data
 const getSalesData = async (req, res) => {
-  const findAllSalesData = await Sales.find({"userID": req.params.userID})
-    .sort({ _id: -1 })
-    .populate("ProductID")
-    .populate("StoreID"); // -1 for descending order
-  res.json(findAllSalesData);
-};
-
-// Get total sales amount
-const getTotalSalesAmount = async(req,res) => {
-  let totalSaleAmount = 0;
-  const salesData = await Sales.find({"userID": req.params.userID});
-  salesData.forEach((sale)=>{
-    totalSaleAmount += sale.TotalSaleAmount;
-  })
-  res.json({totalSaleAmount});
-
-}
-
-const getMonthlySales = async (req, res) => {
   try {
-    const sales = await Sales.find();
+    const { merchant_id } = req.params;
 
-    // Initialize array with 12 zeros
-    const salesAmount = [];
-    salesAmount.length = 12;
-    salesAmount.fill(0)
+    // Find sales data based on the provided merchant_id and sort by date_sold in descending order
+    const salesData = await Sales.find({ merchant_id }).sort({ date_sold: -1 });
 
-    sales.forEach((sale) => {
-      const monthIndex = parseInt(sale.SaleDate.split("-")[1]) - 1;
-
-      salesAmount[monthIndex] += sale.TotalSaleAmount;
+    // Preprocess dates to make them sortable
+    salesData.forEach(sale => {
+      const [month, day, year] = sale.date_sold.split('/');
+      const dateObj = new Date(`${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`);
+      sale.sortable_date = dateObj;
     });
 
-    res.status(200).json({ salesAmount });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
+    // Sort based on the preprocessed dates
+    salesData.sort((a, b) => b.sortable_date - a.sortable_date);
+
+    // Remove the temporary sortable_date field from the response
+    salesData.forEach(sale => delete sale.sortable_date);
+
+    // Return the sorted sales data
+    res.status(200).json({ salesData });
+  } catch (error) {
+    console.error('Error fetching sales data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
-const getTotalProfit = async(req,res) => {
-  let totalSaleAmount = 0;
-  const salesData = await Sales.find({"userID": req.params.userID});
-  salesData.forEach((sale)=>{
-    totalSaleAmount += sale.TotalSaleAmount;
-  })
-  let totalPurchaseAmount = 0;
-  const purchaseData = await Purchase.find({"userID": req.params.userID});
-  purchaseData.forEach((purchase) => {
-    totalPurchaseAmount += purchase.TotalPurchaseAmount;
-  })
-  let totalProfit=totalSaleAmount-totalPurchaseAmount;
-  res.json( {totalProfit} );
-}
+// Get total sales amount
+const getTotalSalesAmount = async (req, res) => {
+  try {
+    const { merchant_id } = req.params;
 
-module.exports = { addSales, getMonthlySales, getSalesData,  getTotalSalesAmount, getTotalProfit};
+    // Find sales data based on the provided merchant_id
+    const salesData = await Sales.find({ merchant_id });
+
+    // Calculate total sales amount
+    const totalSalesAmount = salesData.reduce((total, sale) => total + sale.total_selling_price, 0);
+
+    // Return the total sales amount
+    res.status(200).json({ merchant_id:merchant_id, Total_Sales_Amount: totalSalesAmount });
+  } catch (error) {
+    console.error('Error calculating total sales amount:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const getMonthlySales = async (req, res) => {
+  try {
+    console.log(req.body);
+    const merchant_id = req.body.merchant_id;
+    const month = getMonthNumber(req.body.month);
+    if (!month) {
+      res.json({ error: "Wrong month name!!" });
+      return; // Return to prevent further execution
+    }
+
+    const filteredSales = await Sales.find({
+      merchant_id: merchant_id,
+      date_sold: {
+        $regex: `(?:^|[^0-9])${month}(?:\/|$)`
+      }
+    });
+
+    const totalMonthlyAmount = filteredSales.reduce((total, sale) => total + sale.total_selling_price, 0);
+
+    res.status(200).json({ Monthly_Sale_Amount:totalMonthlyAmount , Sales_Data:filteredSales });
+  } catch (error) {
+    console.error('Error calculating Monthly Sales:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const getTotalProfit = async (req, res) => {
+  try {
+    const { merchant_id } = req.params;
+
+    // Calculate total selling price
+    const totalSellingPrice = await Sales.aggregate([
+      { $match: { merchant_id } },
+      { $group: { _id: null, total: { $sum: '$total_selling_price' } } }
+    ]);
+
+    // Calculate total cost price
+    const totalCostPrice = await Purchase.aggregate([
+      { $match: { merchant_id } },
+      { $group: { _id: null, total: { $sum: '$total_cost_price' } } }
+    ]);
+
+    const totalSelling = totalSellingPrice.length ? totalSellingPrice[0].total : 0;
+    const totalCost = totalCostPrice.length ? totalCostPrice[0].total : 0;
+
+    // Calculate profit
+    const totalProfit = totalSelling - totalCost;
+
+    res.status(200).json({ Total_SP:totalSelling,Total_CP:totalCost,Total_Profit:totalProfit });
+  } catch (error) {
+    console.error('Error calculating total profit:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const getProfitByDay=async (req, res) => {
+  try {
+    const { merchant_id, date } = req.body;
+
+    // Retrieve sales data from 'customer_sales'
+    const customerSales = await Sales.find({
+      merchant_id: merchant_id,
+      date_sold: date
+    });
+    console.log("Customer Sales:",customerSales)
+
+    // Collect product IDs and selling price per unit
+    const productSellPrice = {};
+    customerSales.forEach((sale) => {
+      productSellPrice[sale.product_id] = sale.selling_price_per_unit;
+    });
+
+    // Retrieve purchase data from 'merchant_purchases' based on product IDs
+    const products = Object.keys(productSellPrice);
+    const productCostPrice = {};
+
+    for (const product_id of products) {
+      const costPriceData = await Purchase.findOne({
+        merchant_id: merchant_id,
+        product_id: product_id
+      });
+
+      if (costPriceData) {
+        productCostPrice[product_id] = costPriceData.cost_price_per_unit;
+      } else {
+        console.log(`No cost price data found for product ID: ${product_id}`);
+      }
+    }
+
+    // Calculate profit for each product and accumulate total profit
+    const profits = [];
+    let totalProfit = 0;
+
+    for (const product_id in productSellPrice) {
+      if (productCostPrice[product_id] !== undefined) {
+        const unitsSold = customerSales.find(sale => sale.product_id === product_id).units_sold;
+        const profit = (productSellPrice[product_id] - productCostPrice[product_id]) * unitsSold;
+        totalProfit += profit;
+        profits.push({ product_id: product_id, profit: profit.toFixed(2) });
+      }
+    }
+
+    res.status(200).json({ profits: profits, total_profit: totalProfit.toFixed(2) });
+  } catch (error) {
+    console.error('Error while calculating profit:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const getProfitByMonth=async (req, res) => {
+  try {
+    const merchant_id = req.body.merchant_id;
+    const month = getMonthNumber(req.body.month);
+    console.log(merchant_id, month);
+    if (!month) {
+      res.json({ error: "Wrong month name!!" });
+      return; // Return to prevent further execution
+    }
+
+    const filteredSales = await Sales.find({
+      merchant_id: merchant_id,
+      date_sold: {
+        $regex: `(?:^|[^0-9])${month}(?:\/|$)`
+      }
+    });
+
+    console.log('Filtered Sales:', filteredSales);
+
+    const productIDs = filteredSales.map(sale => sale.product_id);
+
+    const merchantPurchases = await Purchase.find({
+      merchant_id: merchant_id,
+      product_id: { $in: productIDs }
+    });
+
+    console.log('Product Ids and merchant_purchases:', productIDs, merchantPurchases);
+
+    const profits = filteredSales.map(sale => {
+      const correspondingPurchase = merchantPurchases.find(purchase => purchase.product_id === sale.product_id);
+
+      if (correspondingPurchase) {
+        const profit = ((sale.selling_price_per_unit - correspondingPurchase.cost_price_per_unit) * sale.units_sold).toFixed(2);
+        console.log("Profit:", profit);
+        return {
+          product_id: sale.product_id,
+          profit: profit
+        };
+      } else {
+        return null;
+      }
+    }).filter(Boolean);
+
+    const totalProfit = profits.reduce((acc, curr) => acc + parseFloat(curr.profit), 0).toFixed(2);
+
+    res.json({ profits: profits, total_profit: totalProfit });
+  } catch (error) {
+    console.error('Error while finding profit:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const getAllSales=async (req, res) => {
+  try {
+      // const customerSalesCollection = db.collection("customer_sales");
+      
+      // Fetch all documents sorted by date_sold in descending order
+      const sortedSales = await Sales.find()
+          .sort({ date_sold: -1 }); // Sorting by date_sold in descending order
+
+      res.json(sortedSales); // Return the sorted documents as a JSON response
+  } catch (error) {
+      console.error('Error while fetching customer sales:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+module.exports = { addSales, getMonthlySales, getSalesData,  getTotalSalesAmount, getTotalProfit, getProfitByDay, getProfitByMonth, getAllSales};
--- router/sales.js
+++ router/sales.js
@@ -31,12 +31,12 @@ function getMonthNumber(monthName) {
 router.post("/add", sales.addSales);
 
 // Get All Sales
-router.get("/get/:userID", sales.getSalesData);
-router.get("/getmonthly", sales.getMonthlySales);
+router.get("/get/:merchant_id", sales.getSalesData);
+router.get("/get/monthly_sales/:merchant_id", sales.getMonthlySales);
 
 
-router.get("/get/:userID/totalsaleamount", sales.getTotalSalesAmount);
-router.get("/get/:userID/totalprofit", sales.getTotalProfit);
+router.get("/get/totalsaleamount/:merchant_id", sales.getTotalSalesAmount);
+router.get("/get/totalprofit/:merchant_id", sales.getTotalProfit);
 
 //Get Profit based on Day
 //Request Body:
@@ -44,59 +44,7 @@ router.get("/get/:userID/totalprofit", sales.getTotalProfit);
 //   "merchant_id":"m50",
 //   "date":"11/8/2023"
 // }
-router.post('/profit_by_day', async (req, res) => {
-  try {
-    const { merchant_id, date } = req.body;
-
-    // Retrieve sales data from 'customer_sales'
-    const customerSales = await Sales.find({
-      merchant_id: merchant_id,
-      date_sold: date
-    });
-    console.log("Customer Sales:",customerSales)
-
-    // Collect product IDs and selling price per unit
-    const productSellPrice = {};
-    customerSales.forEach((sale) => {
-      productSellPrice[sale.product_id] = sale.selling_price_per_unit;
-    });
-
-    // Retrieve purchase data from 'merchant_purchases' based on product IDs
-    const products = Object.keys(productSellPrice);
-    const productCostPrice = {};
-
-    for (const product_id of products) {
-      const costPriceData = await Purchase.findOne({
-        merchant_id: merchant_id,
-        product_id: product_id
-      });
-
-      if (costPriceData) {
-        productCostPrice[product_id] = costPriceData.cost_price_per_unit;
-      } else {
-        console.log(`No cost price data found for product ID: ${product_id}`);
-      }
-    }
-
-    // Calculate profit for each product and accumulate total profit
-    const profits = [];
-    let totalProfit = 0;
-
-    for (const product_id in productSellPrice) {
-      if (productCostPrice[product_id] !== undefined) {
-        const unitsSold = customerSales.find(sale => sale.product_id === product_id).units_sold;
-        const profit = (productSellPrice[product_id] - productCostPrice[product_id]) * unitsSold;
-        totalProfit += profit;
-        profits.push({ product_id: product_id, profit: profit.toFixed(2) });
-      }
-    }
-
-    res.status(200).json({ profits: profits, total_profit: totalProfit.toFixed(2) });
-  } catch (error) {
-    console.error('Error while calculating profit:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+router.post('/profit_by_day', sales.getProfitByDay);
 
 
 //Get monthly profit
@@ -105,72 +53,9 @@ router.post('/profit_by_day', async (req, res) => {
 //   "merchant_id":"m50",
 //   "month":"November"
 // }
-router.post("/monthly_profit", async (req, res) => {
-    try {
-      const merchant_id = req.body.merchant_id;
-      const month = getMonthNumber(req.body.month);
-      console.log(merchant_id, month);
-      if (!month) {
-        res.json({ error: "Wrong month name!!" });
-        return; // Return to prevent further execution
-      }
-  
-      const filteredSales = await Sales.find({
-        merchant_id: merchant_id,
-        date_sold: {
-          $regex: `(?:^|[^0-9])${month}(?:\/|$)`
-        }
-      });
-  
-      console.log('Filtered Sales:', filteredSales);
-  
-      const productIDs = filteredSales.map(sale => sale.product_id);
-  
-      const merchantPurchases = await Purchase.find({
-        merchant_id: merchant_id,
-        product_id: { $in: productIDs }
-      });
-  
-      console.log('Product Ids and merchant_purchases:', productIDs, merchantPurchases);
-  
-      const profits = filteredSales.map(sale => {
-        const correspondingPurchase = merchantPurchases.find(purchase => purchase.product_id === sale.product_id);
-  
-        if (correspondingPurchase) {
-          const profit = ((sale.selling_price_per_unit - correspondingPurchase.cost_price_per_unit) * sale.units_sold).toFixed(2);
-          console.log("Profit:", profit);
-          return {
-            product_id: sale.product_id,
-            profit: profit
-          };
-        } else {
-          return null;
-        }
-      }).filter(Boolean);
-  
-      const totalProfit = profits.reduce((acc, curr) => acc + parseFloat(curr.profit), 0).toFixed(2);
-  
-      res.json({ profits: profits, total_profit: totalProfit });
-    } catch (error) {
-      console.error('Error while finding profit:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+router.post("/monthly_profit", sales.getProfitByMonth);
 
-  router.get('/all_sales', async (req, res) => {
-    try {
-        // const customerSalesCollection = db.collection("customer_sales");
-        
-        // Fetch all documents sorted by date_sold in descending order
-        const sortedSales = await Sales.find()
-            .sort({ date_sold: -1 }); // Sorting by date_sold in descending order
-
-        res.json(sortedSales); // Return the sorted documents as a JSON response
-    } catch (error) {
-        console.error('Error while fetching customer sales:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+router.get('/all_sales', sales.getAllSales);
   
 
 module.exports = router;
