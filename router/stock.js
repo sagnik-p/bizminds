30cf79faba041ccfeb1a3afdea52572cc2f588d4
rohits-web03 <rohit.dey302003@gmail.com>
2023-12-17 07:55:30 +0530

    Added route for getting stock(inventory) of a merchant

--- router/stock.js
+++ router/stock.js
@@ -19,4 +19,21 @@ router.get('/inventory', async (req, res) => {
     }
 });
 
+router.get('/inventory/:merchant_id', async (req, res) => {
+    try {
+        // const stockCollection = db.collection("stocks");
+
+        // Regex pattern to match user_id starting with 'm'
+        const merchant_id = req.params.merchant_id; // ^m matches 'm' at the beginning, 'i' makes it case insensitive
+
+        // Query the stock collection for documents for Merchant IDs
+        const merchantStock = await Stock.find({ user_id: merchant_id });
+
+        res.json(merchantStock);
+    } catch (error) {
+        console.error('Error while fetching Merchant Stock:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 module.exports=router;
